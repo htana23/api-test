e7ddfabfa2d2aaaf7e35227d841124b28c223fd6
Hideki Tanaka <htana23@gmail.com>
2015-08-07 14:17:45 +0900

    changed itune_param

--- app/models/itune.rb
+++ app/models/itune.rb
@@ -4,26 +4,31 @@ require 'json'
 
 class Itune
   include ActiveModel::Model
-  attr_accessor :term, :limit
+  attr_accessor :term
+
+  def limit
+    @limit
+  end
 
   def find(params)
     url = "https://itunes.apple.com/search"
     uri = URI.parse(url)
-    uri.query = {
-        country: "JP",
-        entity: "musicTrack",
-        term: params[:term],
-        limit: params[:limit],
-        lang: "ja_jp",
-    }.to_param
+    uri.query = itune_param(params)
     result = get_json(uri)
-    ary = result["results"]
-    ary.each do |track|
-      p track["artistName"]
-    end
+    result["results"]
   end
 
   private
+    def itune_param(param)
+      base = {
+        country: "JP",
+        entity: "musicTrack",
+        lang: "ja_jp",
+      }
+      @limit = param[:limit]
+      base.merge(param).to_param
+    end
+
     def get_json(uri)
       json = Net::HTTP.get(uri)
       JSON.parse(json)
