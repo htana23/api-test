51d896b558477549235bbb3c2aa958c0c5fcbbb0
Hideki Tanaka <htana23@gmail.com>
2015-08-20 16:38:26 +0900

    photosou機能追加中

--- app/controllers/photozous_controller.rb
+++ app/controllers/photozous_controller.rb
@@ -1,4 +1,25 @@
 class PhotozousController < ApplicationController
+	before_action :set_photozou
+
 	def show
+		@results = {}
+	end
+
+	def search
+		@results = @itune.find(itune_params)
+		if @results
+			render :index
+		else
+			redirect_to itunes_path
+		end
+	end
+
+	private
+	def itune_params
+		params.require(:itune).permit(:term, :limit, :entity)
+	end
+
+	def set_photozou
+		@photozou = Photozou.new
 	end
 end
